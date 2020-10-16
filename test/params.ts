414672cdcbae181069f0608c7d9a19d88882c53d
Jun Kurihara <kurihara@ieee.org>
2020-10-16 17:28:28 +0900

    fix: remove circleci cache

--- test/params.ts
+++ test/params.ts
@@ -5,16 +5,6 @@ let user_id: string;
 let password: string;
 let region_name: string;
 let host_name: string;
-console.log(process.env);
-
-
-pool_id = (process.env.POOL_ID) ? process.env.POOL_ID : '';
-client_id = (process.env.CLIENT_ID) ? process.env.CLIENT_ID : '';
-federation_id = (process.env.FEDERATION_ID)? process.env.FEDERATION_ID : '';
-user_id = (process.env.USER_ID) ? process.env.USER_ID: '';
-password = (process.env.PASSWORD) ? process.env.PASSWORD: '';
-region_name = (process.env.REGION_NAME) ? process.env.REGION_NAME: '';
-host_name = (process.env.HOST_NAME) ? process.env.HOST_NAME: '';
 
 try {
   const authParams = require('./auth-params');
@@ -25,10 +15,15 @@ try {
   password = authParams.password;
   region_name = authParams.region_name;
   host_name = authParams.host_name;
-  console.log('Installed from auth-params.ts');
 }
 catch (e) {
-  console.log('Installed from Environment variables');
+  pool_id = (process.env.POOL_ID) ? process.env.POOL_ID : '';
+  client_id = (process.env.CLIENT_ID) ? process.env.CLIENT_ID : '';
+  federation_id = (process.env.FEDERATION_ID)? process.env.FEDERATION_ID : '';
+  user_id = (process.env.USER_ID) ? process.env.USER_ID: '';
+  password = (process.env.PASSWORD) ? process.env.PASSWORD: '';
+  region_name = (process.env.REGION_NAME) ? process.env.REGION_NAME: '';
+  host_name = (process.env.HOST_NAME) ? process.env.HOST_NAME: '';
 }
 
 export {pool_id, client_id, federation_id, user_id, password, region_name, host_name};
