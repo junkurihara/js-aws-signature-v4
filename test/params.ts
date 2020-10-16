cba92d9124228ec23b549be9e4ec832e0cae32c1
Jun Kurihara <kurihara@ieee.org>
2020-10-16 18:01:15 +0900

    use dotenv for auth params

--- test/params.ts
+++ test/params.ts
@@ -1,29 +1,12 @@
-let pool_id: string;
-let client_id: string;
-let federation_id: string;
-let user_id: string;
-let password: string;
-let region_name: string;
-let host_name: string;
+require('dotenv').config();
 
-try {
-  const authParams = require('./auth-params');
-  pool_id = authParams.pool_id;
-  client_id = authParams.client_id;
-  federation_id = authParams.federation_id;
-  user_id = authParams.user_id;
-  password = authParams.password;
-  region_name = authParams.region_name;
-  host_name = authParams.host_name;
-}
-catch (e) {
-  pool_id = (process.env.POOL_ID) ? process.env.POOL_ID : '';
-  client_id = (process.env.CLIENT_ID) ? process.env.CLIENT_ID : '';
-  federation_id = (process.env.FEDERATION_ID)? process.env.FEDERATION_ID : '';
-  user_id = (process.env.USER_ID) ? process.env.USER_ID: '';
-  password = (process.env.PASSWORD) ? process.env.PASSWORD: '';
-  region_name = (process.env.REGION_NAME) ? process.env.REGION_NAME: '';
-  host_name = (process.env.HOST_NAME) ? process.env.HOST_NAME: '';
-}
+const pool_id = (process.env.POOL_ID) ? process.env.POOL_ID : '';
+const client_id = (process.env.CLIENT_ID) ? process.env.CLIENT_ID : '';
+const federation_id = (process.env.FEDERATION_ID)? process.env.FEDERATION_ID : '';
+const user_id = (process.env.USER_ID) ? process.env.USER_ID: '';
+const password = (process.env.PASSWORD) ? process.env.PASSWORD: '';
+const region_name = (process.env.REGION_NAME) ? process.env.REGION_NAME: '';
+const host_name = (process.env.HOST_NAME) ? process.env.HOST_NAME: '';
 
+console.log(pool_id, client_id, federation_id, user_id, password, region_name, host_name);
 export {pool_id, client_id, federation_id, user_id, password, region_name, host_name};
